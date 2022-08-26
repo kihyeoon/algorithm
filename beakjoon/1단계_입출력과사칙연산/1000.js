8412a37ec1030ef1248ac7d15414913338b02a89
kihyeoon <bangion93@gmail.com>
2022-08-26 23:43:48 +0900

    [BOJ] 소트인사이드 / 실버5 / 5분
    https://www.acmicpc.net/problem/1427

--- beakjoon/1단계_입출력과사칙연산/1000.js
+++ beakjoon/1단계_입출력과사칙연산/1000.js
@@ -1,6 +1,14 @@
-let fs = require("fs");
-let input = fs.readFileSync("1000.txt").toString().split(" ");
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : __filename.split("/").pop().slice(0, -3) + ".txt" // 1000.txt
+  )
+  .toString()
+  .trim()
+  .split(" ");
 
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
+
 console.log(a + b);
