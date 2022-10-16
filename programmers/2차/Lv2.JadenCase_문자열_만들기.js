ce287115e60a092056674d58805ba81c7fcb77b7
kihyeoon <bangion93@gmail.com>
2022-10-16 15:55:30 +0900

    ♻️ refactor code

--- programmers/2차/Lv2.JadenCase_문자열_만들기.js
+++ programmers/2차/Lv2.JadenCase_문자열_만들기.js
@@ -1,10 +1,8 @@
 function solution(s) {
-  var answer = "";
-  answer = s
+  return s
     .split(" ")
     .map(
       (word) => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
     )
     .join(" ");
-  return answer;
 }
