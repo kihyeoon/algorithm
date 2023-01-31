67875810059b1cc2921d77b2fec6d3954fd2e192
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-01-31 11:13:42 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,1 @@
-function solution(n) {
-  let answer = '';
-  for (let i = 0; i < n; i++) {
-    answer += i % 2 === 0 ? '수' : '박';
-  }
-  return answer;
-}
-
+const solution = (n) => "수박".repeat(n / 2) + (n % 2 ? "수" : "");
