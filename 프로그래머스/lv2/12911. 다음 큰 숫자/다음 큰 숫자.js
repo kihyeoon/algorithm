ca436bf4f4309b2bc1cdeaead3e68eb4e63aa5d3
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-02-01 14:44:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,5 +1,8 @@
-function solution(n, a = n + 1) {
-  return n.toString(2).match(/1/g).length === a.toString(2).match(/1/g).length
-    ? a
-    : solution(n, a + 1);
+function solution(n) {
+    let answer = n + 1;
+    let length = n.toString(2).replace(/[0]/g, '').length
+    while (answer.toString(2).replace(/[0]/g, '').length != length) {
+        answer++
+    }
+    return answer;
 }
