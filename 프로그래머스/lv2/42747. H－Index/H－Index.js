299d3647ff0bfa731abcb237a90145d28531fe6a
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-01 12:08:11 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,7 +1,6 @@
 function solution(citations) {
   citations.sort((a, b) => b - a);
 
-  let answer = 0;
   for (let i = citations.length - 1; i >= 0; i--) {
     if (i >= citations[i]) citations.pop();
   }
