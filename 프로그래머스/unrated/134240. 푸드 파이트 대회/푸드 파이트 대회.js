c15bd1350bf292050b7ea017725a133ac34fec32
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-08 00:10:28 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,8 +1,13 @@
-function solution(food) {
-    let res = '';
-    for (let i = 1; i < food.length; i++) {
-        res += String(i).repeat(Math.floor(food[i]/2));
-    }
+function solution(foods) {
+  const answer = [];
+  let food = 0;
 
-    return res + '0' + [...res].reverse().join('');
+  for (let i = 1; i < foods.length; i++) {
+    food = foods[i] % 2 ? foods[i] - 1 : foods[i];
+    if (!food) continue;
+
+    answer.push(i.toString().repeat(food / 2));
+  }
+
+  return [...answer, "0", ...[...answer].reverse()].join("");
 }
