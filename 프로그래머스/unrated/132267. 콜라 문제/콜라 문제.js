b664035742228d3e4e6c35da1b727a0847d07583
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-07 12:32:11 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,13 +1,10 @@
 function solution(a, b, n) {
   let answer = 0;
-  let 나머지, coke;
-
+    
   while (n >= a) {
-    나머지 = n % a;
-    coke = ((n - 나머지) / a) * b;
-    answer += coke;
-    n = 나머지 + coke;
+    answer += Math.floor(n / a) * b;
+    n = Math.floor(n / a) * b + (n % a);
   }
-
+    
   return answer;
 }
