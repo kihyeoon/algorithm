023f623160c874250f75b6839041007657cc1ae2
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-08 00:00:51 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,16 @@
+function solution(foods) {
+  const answer = [];
+  let food = 0;
+
+  for (let i = 1; i < foods.length; i++) {
+    food = foods[i] % 2 ? foods[i] - 1 : foods[i];
+    if (!food) continue;
+
+    answer.push(i.toString().repeat(food / 2));
+  }
+
+  let reverse = [...answer].reverse();
+  answer.push("0");
+
+  return [...answer, ...reverse].join("");
+}
