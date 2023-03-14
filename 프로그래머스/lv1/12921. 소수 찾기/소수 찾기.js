6f935dabab066ce0e134ae46b8465ec456ffc4d4
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-14 22:17:52 +0900

    [level 1] Title: 소수 찾기, Time: 28.74 ms, Memory: 45.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,15 +1,8 @@
 function solution(n) {
-  let numbers = new Array(n + 1).fill(true);
-  numbers[0] = false;
-  numbers[1] = false;
+let numbers = new Array(n + 1).fill(true, 2);
 
-  // 합성수는 false로 교체
   for (let i = 2; i < Math.sqrt(n); i++) {
     if (numbers[i]) {
-      // 합성수는 기준수의 배수로 찾는다.
-      // 시작을 기준수의 제곱으로 한다.
-      // (본인 수 제외, 제곱 값 이전의 수는 기준수보다 작은 소수가 시행될 때 이미 걸러짐)
-      // 판단해야 할 수는 기준수의 배수이므로, 기준 수를 더해가며 건너 뛴다.
       for (let j = i * i; j <= n; j += i) {
         numbers[j] = false;
       }
