dae8a37e3c4bb5807c0cef7d74b574a40dc7744a
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-14 19:10:01 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.02 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,3 +1,6 @@
 function solution(s) {
-  return [...s].map((el, i) => [...s.slice(0, i), ""].reverse().indexOf(el));
+  return [...s].map((el, i) => {
+    const count = s.slice(0, i).lastIndexOf(el);
+    return count === -1 ? count : i - count;
+  });
 }
