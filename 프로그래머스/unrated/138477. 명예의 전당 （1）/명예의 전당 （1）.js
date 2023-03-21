d23c44f77e5b41c8660e5a4cdff8975d4a2b4f10
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-03-21 18:03:57 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,9 +1,12 @@
 function solution(k, score) {
   let answer = [];
+  let sortedScore;
 
   for (let i = 1; i <= score.length; i++) {
-    let HallOfFame = score.slice(0, i).sort((a, b) => b - a);
-    answer.push(HallOfFame.length < k ? HallOfFame.pop() : HallOfFame[k - 1]);
+    sortedScore = score.slice(0, i).sort((a, b) => b - a);
+    answer.push(
+      sortedScore.length < k ? sortedScore.pop() : sortedScore[k - 1]
+    );
   }
 
   return answer;
