fd4aac6f248883ca0944bbeab86858d32791fa09
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-04-11 17:39:14 +0900

    [unrated] Title: 옹알이 (2), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,33 +1,4 @@
 function solution(babbling) {
-  const pronounce = ["aya", "ye", "woo", "ma"];
-  let count = 0;
-
-  babbling.forEach((word) => {
-    let isPossible = true;
-    let prevPronounce = "";
-
-    for (let i = 0; i < word.length; ) {
-      let found = false;
-
-      for (const p of pronounce) {
-        if (word.startsWith(p, i) && prevPronounce !== p) {
-          found = true;
-          prevPronounce = p;
-          i += p.length;
-          break;
-        }
-      }
-
-      if (!found) {
-        isPossible = false;
-        break;
-      }
-    }
-
-    if (isPossible) {
-      count++;
-    }
-  });
-
-  return count;
+    const regex = /^(?:(aya|ye|woo|ma)(?!\1))*$/;
+    return babbling.filter(word => regex.test(word)).length;
 }
