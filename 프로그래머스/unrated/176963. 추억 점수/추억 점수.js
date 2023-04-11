febebc9823d169fb71f6aec0948d4c9fa6632ceb
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-04-11 17:24:17 +0900

    [unrated] Title: 추억 점수, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -4,11 +4,9 @@ function solution(name, yearning, photo) {
     return acc;
   }, {});
 
-  const result = photo.map((photoPeople) => {
+  return result = photo.map((photoPeople) => {
     return photoPeople.reduce((acc, person) => {
       return acc + (hash[person] || 0);
     }, 0);
   });
-
-  return result;
 }
