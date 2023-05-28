4150584698ddd0f5e2a906de7685e06d7c6ac31b
방기현 <102677317+kihyeoon@users.noreply.github.com>
2023-05-28 16:32:26 +0900

    [level 2] Title: 피로도, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,15 +1,20 @@
-function solution(k, dungeons) {
-  let answer = -1;
-  const dfs = (k, dungeons, prev) => {
-    for (let i = 0; i < dungeons.length; i++) {
-      const [req, use] = dungeons[i];
-      if (!req || req > k) continue;
-      const copy = [...dungeons].map((v) => [...v]);
-      copy[i] = [null, null];
-      dfs(k - use, copy, prev + 1);
+function solution(k, d) {
+    const N = d.length
+    const visited = new Array(N).fill(0)
+    let ans = 0
+
+    function dfs(k, cnt){
+        ans = Math.max(cnt, ans)
+
+        for (let j = 0; j < N; j++){
+            if (k >= d[j][0] && !visited[j]){
+                visited[j] = 1
+                dfs(k - d[j][1], cnt + 1)
+                visited[j] = 0
+            }
+        }
     }
-    return (answer = Math.max(prev, answer));
-  };
-  dfs(k, dungeons, 0);
-  return answer;
+
+    dfs(k, 0)
+    return ans;
 }
